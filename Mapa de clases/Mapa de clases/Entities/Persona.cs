3ffb170a7db330282a6f3787f38b0ed5fd749546
IsaelBesonias <151092260+IsaelBesonias@users.noreply.github.com>
2024-10-05 00:49:19 -0400

    Add files via upload

---
+++ Mapa de clases/Mapa de clases/Entities/Persona.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Mapa_de_clases.Entities;
+
+namespace Mapa_de_clases.Entities
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Ocupacion { get; set; }
+
+        public Persona(string nombre, string apellido, string ocupacion) {
+            Nombre = nombre;
+            Apellido = apellido;
+            Ocupacion = ocupacion;
+        }
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre} {Apellido}, Ocupación: {Ocupacion}");
+        }
+
+    }
+}
+
+public class MiembroComunidad : Persona
+{
+    public MiembroComunidad(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+
+    }
+}
+
+public class Empleado : Persona
+{
+    public Empleado(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
+
+public class Estudiante : Persona
+{
+    public Estudiante(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
+
+public class ExAlumno : Persona
+{
+    public ExAlumno(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
+
+public class Docente : Persona
+{
+    public Docente(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
+
+public class Administrativo : Persona
+{
+    public Administrativo(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
+
+public class Administrador : Persona
+{
+    public Administrador(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
+
+public class Maestro : Persona
+{
+    public Maestro(string nombre, string apellido, string ocupacion) : base(nombre, apellido, ocupacion)
+    {
+    }
+}
---
+++ Mapa de clases/Mapa de clases/Program.cs
@@ -0,0 +1,25 @@
+﻿using Mapa_de_clases.Entities;
+
+var miembrocomunidad = new MiembroComunidad("Carlos", "Ramírez", "Servidor Publico");
+miembrocomunidad.MostrarInformacion();
+
+var empleado = new Empleado("Luis","Perez", "Abogado" );
+empleado.MostrarInformacion();
+
+var estudiante = new Estudiante("Juan", "Lopez", "Desarrollo de software");
+estudiante.MostrarInformacion();
+
+var exalumno = new ExAlumno("Manuel", "Pereira", "Taxista");
+exalumno.MostrarInformacion();
+
+var docente = new Docente("Rafael", "Santana", "Profesor de sociales");
+docente.MostrarInformacion();
+
+var administrativo = new Administrativo("Brander", "Baez", "Funcionario");
+administrativo.MostrarInformacion();
+
+var administrador = new Administrador("David", "Baez", "Administrador de edesur");
+administrador.MostrarInformacion();
+
+var maestro = new Maestro("Bruce", "Lee", "Maestro en las artes marciales");
+maestro.MostrarInformacion();
