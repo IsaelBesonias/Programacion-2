5428cc418466935e738be8ffab470098ba61b2f2
IsaelBesonias <151092260+IsaelBesonias@users.noreply.github.com>
2024-10-26 21:14:51 -0400

    Add files via upload

---
+++ Tarea 3/RegistroEventosDeportivos.Domain/Entities/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Entidades/Event.cs
+namespace RegistroEventosDeportivos.Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+
+        public List<Athlete> Athletes { get; set; } = new List<Athlete>();
+    }
+}
+
+// Entidades/Athlete.cs
+namespace RegistroEventosDeportivos.Domain.Entities
+{
+    public class Athlete
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sport { get; set; }
+    }
+}
