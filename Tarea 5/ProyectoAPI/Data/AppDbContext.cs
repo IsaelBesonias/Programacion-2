edbb19736acc97ca529e065c510b442adf441ef8
IsaelBesonias <151092260+IsaelBesonias@users.noreply.github.com>
2024-11-02 20:45:30 -0400

    Add files via upload

---
+++ Tarea 5/ProyectoAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ProyectoAPI.Models;
+
+namespace ProyectoAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
